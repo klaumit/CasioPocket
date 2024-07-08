a102382d75a999eb55ac1fadee536ccfcf69b7bb
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-07-08 21:03:51 +0200

    Correcting paths

--- Vendor/PathSet.bat
+++ Vendor/PathSet.bat
@@ -1,5 +1,7 @@
 SET TZ=JST-9
-SET PATH=%~dp0lsij\lsic86pv\bin;%~dp0CASIO\PV2EN02\TOOL;
-SET MAKEDEFAULT=%~dp0lsij\lsic86pv\bin\makedef
-SET PVC=%~dp0CASIO\PV2EN02\C
+SET LUP=%~dp0lsij\lsic86pv
+SET CUP=%~dp0CASIO\PV2EN02
+SET PATH=%LUP%\bin;%CUP%\TOOL;
+SET MAKEDEFAULT=%LUP%\bin\makedef
+SET PVC=%CUP%\C
 
