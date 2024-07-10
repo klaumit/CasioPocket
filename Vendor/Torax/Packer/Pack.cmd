dfbf5e1480e480ee0093f9de1339280d566fccb1
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-07-11 00:29:57 +0200

    Adding ebook packer

---
+++ Vendor/Torax/Packer/Pack.cmd
@@ -0,0 +1,2 @@
+"%homepath%\Desktop\eBooks\Packer\LHA32" a "%homepath%\Desktop\eBooks\ebook.lzh" "%homepath%\Desktop\eBooks\*.txt"
+"%homepath%\Desktop\eBooks\adtgui.exe" "%homepath%\Desktop\eBooks\ebook.lzh"
