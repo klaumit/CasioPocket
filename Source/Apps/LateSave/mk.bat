4d556e1a2541353433e6a8ceced34e1d1115e799
Klaus Mittermann <klaus.mittermann@test.com.uk>
2024-07-10 18:09:58 +0200

    Fixing build

--- Source/Apps/LateSave/mk.bat
+++ Source/Apps/LateSave/mk.bat
@@ -1,9 +1,9 @@
 @echo off
-CALL ..\..\PATHSET.BAT
+CALL ..\..\..\Vendor\PATHSET.BAT
 @echo\
 @echo --- MAKE START ---
-kmmake -f MAKEFILE PMODEL=1 %1 %2 %3
+kmmake PMODEL=1
 if exist make.i del make.i
 @echo --- MAKE START (ForDebug)---
-kmmake -f MAKEFILE PMODEL=1 DEBUG=1  %1 %2 %3 > err
+kmmake PMODEL=1 DEBUG=1 > err
 if exist make.i del make.i
