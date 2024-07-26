c5cfbc4aea32b52f8176093167a7e6c475315f51
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-27 00:44:27 +0200

    Update PathSet.bat

--- Vendor/PathSet.bat
+++ Vendor/PathSet.bat
@@ -4,8 +4,9 @@ SET CUP=%~dp0CASIO\PV2EN02
 SET PATH=%LUP%\bin;%CUP%\TOOL;
 SET MAKEDEFAULT=%LUP%\bin\makedef
 SET PVC=%CUP%\C
-mkdir ..\Bin
 mkdir ..\Obj
+mkdir ..\ForDEBUG
+mkdir ..\Bin
 mkdir ..\User_Bin
 mkdir .\obj
 mkdir .\ForDEBUG
