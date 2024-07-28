83c7ce2eb2bc39d9041f85d372eeaf8b228bb99e
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-28 16:04:36 +0200

    Adding new option to debug

--- Source/Apps/AssTest/mk.bat
+++ Source/Apps/AssTest/mk.bat
@@ -1,6 +1,18 @@
 @echo off
 CALL ..\..\..\Vendor\PATHSET.BAT
 @echo\
+
+@echo --- MAKE MAIN ---
+copy /y v_main\makefile
+@echo --- MAKE START ---
+kmmake PMODEL=1
+if exist make.i del make.i
+@echo --- MAKE START (ForDebug)---
+kmmake PMODEL=1 DEBUG=1 > err
+if exist make.i del make.i
+
+@echo --- MAKE FULL ---
+copy /y v_full\makefile
 @echo --- MAKE START ---
 kmmake PMODEL=1
 if exist make.i del make.i
