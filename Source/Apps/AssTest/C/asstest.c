aba484fefed37a6ef4454577b9d3ce830d0ec527
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-29 20:48:21 +0200

    Update asstest.c

--- Source/Apps/AssTest/C/asstest.c
+++ Source/Apps/AssTest/C/asstest.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
+#include <string.h>
 #include <define.h>
+#include <l_define.h>
 #include <libc.h>
 #include <l_libc.h>
+#include <stdrom.h>
 
 void main ()
 {
-	int x, y, z, u, w, t;
+	char* str1;
+	int x, y, z, u, w, t, l, a, sum, diff, prod, quot, rem, b, counter, i;
+
 	x = 32469;
 	y = 32468;
 	z = 32467;
@@ -30,5 +36,49 @@ void main ()
 	u = x ^ y;
 	u = ~y;
 	u = x << y;
-	u = x >> y;	
+	u = x >> y;
+
+	b = 99999999;
+
+	a = 10, b = 5;
+    sum = a + b;
+    diff = a - b;
+    prod = a * b;
+    quot = a / b;
+    rem = a % b;
+	l = 3.393938 * 221.2991211;
+
+	a = 0xF0F0, b = 0x0F0F;
+    w = a & b;
+    x = a | b;
+    y = a ^ b;
+    u = ~a;
+
+	counter = 0;
+    for (i = 1; i <= 10; i++) {
+        counter += i;
+    }
+    a = counter;
+
+	x = str1[2];
+	u = y + 324939393;
+
+	while(t < u) w *= 2;
+
+	if (x == 3) 
+		y = 5;
+	else if (x == 5)
+		y = 7;
+	else
+		y = 9;
+
+	if (z == 3)
+		w = w * 3;
+
+	switch (diff) {
+		case 1: return;
+		case 2: prod = 23924; break;
+	}
+	
+	do { w -= 7; } while (z <= u);	
 }
