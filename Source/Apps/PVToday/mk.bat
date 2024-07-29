51aca876475bc477cc4af64749afffd6aa562f0e
Klaus Mittermann <klaus.mittermann@hotmail.com>
2024-07-29 20:13:44 +0200

    Adding opt to makes

--- Source/Apps/PVToday/mk.bat
+++ Source/Apps/PVToday/mk.bat
@@ -2,7 +2,7 @@
 CALL ..\..\..\Vendor\PATHSET.BAT
 @echo\
 @echo --- MAKE START ---
-kmmake PMODEL=1 DNAME=LANG_ENG
+kmmake PMODEL=1 DNAME=LANG_ENG %KM_OPT1% %KM_OPT2%
 if exist make.i del make.i
 @echo --- MAKE START (ForDebug)---
 kmmake PMODEL=1 DNAME=LANG_ENG DEBUG=1 > err
